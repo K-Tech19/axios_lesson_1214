028616611cc7c73a6b28f2e710503d256494c505
nickubed <nhpquandt@gmail.com>
2021-01-12 11:17:27 -0800

    Added form functionality

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ require('dotenv').config()
 const axios = require('axios')
 const express = require('express')
 const app = express()
-// const API_KEY = 'farts'
 const API_KEY = process.env.API_KEY
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-    axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&s=twin+peaks`)
+    res.render('index')
+})
+
+app.get('/search', (req, res) => {
+    console.log(req.query)
+    axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${req.query.searchTerm}`)
     .then(response => {
         res.send(response.data)
     })
